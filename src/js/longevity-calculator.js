34af6f38a1056a404a37e68cf337a2876e649bda
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-11 10:05:04 -0800

    Pass first test, Demographic() returns expected output

--- src/js/longevity-calculator.js
+++ src/js/longevity-calculator.js
@@ -1,5 +1,7 @@
 export class Demographic {
   constructor(geneticFactors, lifestyleNegatives, lifestylePositives) {
-
+    this.geneticFactors = geneticFactors;
+    this.lifestyleNegatives = lifestyleNegatives;
+    this.lifestylePositives = lifestylePositives;
   }
 }
