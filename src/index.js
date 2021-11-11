97feab01fa98688adcdb573bd17677061cc88d62
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-11 12:04:41 -0800

    Add UI for user input and calculations to index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,38 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Demographic } from '../src/js/longevity-calculator';
-import { planets } from '../src/js/age-calculator.js';
 import { calculator } from '../src/js/age-calculator';
+
+function longevityInput() {
+  const genetics = (document.querySelectorAll('input[name="genetics:]:checked')).length;
+  const lifestylePos = (document.querySelectorAll('input[name="lifestyle-pos:]:checked')).length;
+  const lifestyleNeg = (document.querySelectorAll('input[name="lifestyle-neg:]:checked')).length;
+  const userDemographic = new Demographic (genetics, lifestylePos, lifestyleNeg);
+  return userDemographic;
+}
+
+function remainingYears() {
+  const userDemographic = longevityInput();
+  const lifeExpectancy = longevityAdjuster(userDemographic);
+  const age = $("#user-age").val();
+  const planet = $("input:radio[name=planet]:checked").val();
+  const yearsLeft = lifeExpectancy - age;
+  if (age > lifeExpectancy) {
+    yearsLeft *= -1;
+    return `Congrats! You've made it ${yearsLeft} ${planet}-years past your life expectancy on ${planet}!`
+  } else {
+    return `You have ${yearsLeft} ${planet}-years to live on ${planet}.`
+  }
+}
+
+$(document).ready(function() {
+  $("form#user-info").submit(function(event) {
+    event.preventDefault();
+    const name = $("#user-name").val();
+    const age = $("#user-age").val();
+    const planet = $("input:radio[name=planet]:checked").val();
+
+    const ageOnPlanet = calculator(age, planet);
+    const yearsLeft = remainingYears();
+  });
+});
