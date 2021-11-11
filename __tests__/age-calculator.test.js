62be96b495f2e82923feb68601b1b654fe73ecfb
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-11 08:46:15 -0800

    Add age-calculator.js and first test

---
+++ __tests__/age-calculator.test.js
@@ -0,0 +1,11 @@
+import { planets } from '../src/js/age-calculator.js';
+
+describe ('planets', () => {
+  test('Should return information about each planet', () => {
+    expect(planets.get("Earth")).toBe(1);
+    expect(planets.get("Mercury")).toBe(.24);
+    expect(planets.get("Venus")).toBe(.62);
+    expect(planets.get("Mars")).toBe(1.88);
+    expect(planets.get("Jupiter")).toBe(11.86);
+  });
+});
