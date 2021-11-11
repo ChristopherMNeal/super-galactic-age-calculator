f876aaea697086df3439651e62c709ba127652d9
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-11 10:49:24 -0800

    Add imports to index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Rectangle from './js/rectangle.js';
+import { Demographic } from '../src/js/longevity-calculator';
+import { planets } from '../src/js/age-calculator.js';
+import { calculator } from '../src/js/age-calculator';
