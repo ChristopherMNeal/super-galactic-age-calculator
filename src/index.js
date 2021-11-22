7fd5b4486b178b2518a37dd973787adfe25e3b62
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-21 16:25:50 -0800

    Add highlight class to missing input form, WIP refactor to incorporate animalConverter()

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Demographic } from '../src/js/longevity-calculator';
-import { calculator } from '../src/js/age-calculator';
+import { calculator, animalConverter } from '../src/js/age-calculator';
 
 function longevityInput() {
   const genetics = (document.querySelectorAll('input[name="genetics"]:checked')).length;
@@ -15,15 +15,18 @@ function longevityInput() {
 
 function remainingYears() {
   const userDemographic = longevityInput();
-  const lifeExpectancy = userDemographic.longevityAdjuster();
-  const age = $("#user-age").val();
-  const convertedAge = animalYears(age);
-  const convertedLifeExpectancy = animalYears(lifeExpectancy);
+  const animal = $("input:radio[name=animal-years]:checked").val();
+  let age = $("#user-age").val();
+  let lifeExpectancy = userDemographic.longevityAdjuster();
+  // should I just animalConvert yearsLeft? test once i get everything working
+  animalConverter(lifeExpectancy, animal);
+  animalConverter(age, animal);
   const planet = $("input:radio[name=planet]:checked").val();
-  const animalPlanet = getAnimalPlanet();
-  let yearsLeft = convertedLifeExpectancy - convertedAge;
-  let planetYearsLeft = calculator(yearsLeft, planet);
-  if (convertedAge > convertedLifeExpectancy) {
+  let yearsLeft = lifeExpectancy - age;
+  let planetYearsLeft = calculator(yearsLeft, planet, animal);
+  // move this calculation to business logic and the formatting to a separate UI function
+  const animalPlanet= getAnimalPlanet(animal, planet);
+  if (age > lifeExpectancy) {
     planetYearsLeft *= -1;
     return `Congrats! You've made it ${planetYearsLeft} ${animalPlanet} past your life expectancy!`;
   } else {
@@ -31,27 +34,21 @@ function remainingYears() {
   }
 }
 
-function animalYears(age) {
-  if ($("input:radio[name=animal-years]:checked").val() === "dog-years") {
-    return age *= 8;
-  } else if ($("input:radio[name=animal-years]:checked").val() === "mayfly-lifetimes") {
-    return age *= 105120;
-  } else {
-    return age;
-  }
-}
-
-function getAnimalPlanet() {
-  const planet = $("input:radio[name=planet]:checked").val();
-  if ($("input:radio[name=animal-years]:checked").val() === "dog-years") {
+// I left jquery selectors in, in case it's not practial to querey the dom...?
+function getAnimalPlanet(animal, planet) {
+  // const planet = $("input:radio[name=planet]:checked").val();
+  // if ($("input:radio[name=animal-years]:checked").val() === "dog-years") {
+  if (animal === "dog-years") {
     return planet + " dog years";
-  } else if ($("input:radio[name=animal-years]:checked").val() === "mayfly-lifetimes") {
+// } else if ($("input:radio[name=animal-years]:checked").val() === "mayfly-lifetimes") {
+  } else if (animal === "mayfly-lifetimes") {
     return planet + " mayfly lifetimes";
   } else {
     return planet + " years";
   }
 }
 
+// add sunComparison to age-calculator
 function getSunComparison(age, animalPlanet) {
   const ageComparison = parseInt(4603000000/age);
   if ($("input:radio[name=animal-years]:checked").val() === "mayfly-lifetimes") {
@@ -66,9 +63,10 @@ $(document).ready(function() {
     event.preventDefault();
     const name = $("#user-name").val();
     const age = $("#user-age").val();
-    const animalAge = animalYears(age);
+    const animal = $("input:radio[name=animal-years]:checked").val();
+    const convertedAge = animalConverter(age, animal);
     const planet = $("input:radio[name=planet]:checked").val();
-    const ageOnPlanet = calculator(animalAge, planet);
+    const ageOnPlanet = calculator(convertedAge, planet);
     const animalPlanet = getAnimalPlanet();
     const sunComparison = getSunComparison(ageOnPlanet, animalPlanet);
     const yearsLeft = remainingYears();
@@ -78,8 +76,16 @@ $(document).ready(function() {
       $("#too-old").show();
     } else if (age < 0) {
       $("#too-young").show();
-    } else if (!name || !age || !planet) {
+    // could this be more DRY?
+    } else if (!name) {
       $("#no-input").show();
+      $("#user-name").addClass("highlight");
+    } else if (!age) {
+      $("#no-input").show();
+      $("#user-age").addClass("highlight");
+    } else if (!planet) {
+      $("#no-input").show();
+      $("#planet-select").addClass("highlight");
     } else {
       $("#results").show();
       $("#results").html("Here are your results, " + name + "<br>Your age in " + animalPlanet + " is " + ageOnPlanet + ". <br>" + sunComparison + yearsLeft);
